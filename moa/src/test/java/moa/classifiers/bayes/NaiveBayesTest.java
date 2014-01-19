b973d7ce6aa8fec58943a9094b8dd71dc8ceec13
abifet <abifet@gmail.com>
2014-01-19 12:54:24 +0100

    Updated Classifier Tests

--- moa/src/test/java/moa/classifiers/bayes/NaiveBayesTest.java
+++ moa/src/test/java/moa/classifiers/bayes/NaiveBayesTest.java
@@ -21,7 +21,7 @@ package moa.classifiers.bayes;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
-import moa.classifiers.AbstractClassifierTestCase;
+import moa.classifiers.AbstractMultipleClassifierTestCase;
 import moa.classifiers.Classifier;
 import moa.evaluation.BasicClassificationPerformanceEvaluator;
 import moa.evaluation.ClassificationPerformanceEvaluator;
@@ -33,7 +33,7 @@ import moa.evaluation.ClassificationPerformanceEvaluator;
  * @version $Revision$
  */
 public class NaiveBayesTest
-  extends AbstractClassifierTestCase {
+  extends AbstractMultipleClassifierTestCase {
 
   /**
    * Constructs the test case. Called by subclasses.
@@ -41,71 +41,9 @@ public class NaiveBayesTest
    * @param name 	the name of the test
    */
   public NaiveBayesTest(String name) {
-    super(name);
+    super(name, 1);
   }
 
-  /**
-   * Called by JUnit before each test method.
-   *
-   * @throws Exception if an error occurs.
-   */
-  @Override
-  protected void setUp() throws Exception {
-    super.setUp();
-    
-    m_TestHelper.copyResourceToTmp("naivebayes.arff");
-  }
-  
-  /**
-   * Called by JUnit after each test method.
-   *
-   * @throws Exception	if tear-down fails
-   */
-  @Override
-  protected void tearDown() throws Exception {
-    m_TestHelper.deleteFileFromTmp("naivebayes.arff");
-    
-    super.tearDown();
-  }
-
-  /**
-   * Returns the filenames (without path) of the input data files to use
-   * in the regression test.
-   *
-   * @return		the filenames
-   */
-  @Override
-  protected String[] getRegressionInputFiles() {
-    return new String[]{
-	"naivebayes.arff",
-    };
-  }
-
-  /**
-   * Returns the class index for the datasets.
-   * 
-   * @return		the class indices (0-based)
-   */
-  @Override
-  protected int[] getRegressionInputClassIndex() {
-    return new int[]{
-	10,
-    };
-  }
-
-  /**
-   * Returns the index of the instances in the stream to inspect the 
-   * performance/classification output of the classifiers.
-   * 
-   * @return		the inspection indices
-   */
-  @Override
-  protected int[][] getRegressionInspectionPoints() {
-    return new int[][]{
-	new int[]{10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000},
-    };
-  }
-  
   /**
    * Returns the classifier setups to use in the regression test.
    *
@@ -117,19 +55,7 @@ public class NaiveBayesTest
 	new NaiveBayes(),
     };
   }
-
-  /**
-   * Returns the evaluator setups to use in the regression test.
-   *
-   * @return		the setups
-   */
-  @Override
-  protected ClassificationPerformanceEvaluator[] getRegressionEvaluatorSetups() {
-    return new ClassificationPerformanceEvaluator[]{
-	new BasicClassificationPerformanceEvaluator(),
-    };
-  }
-
+  
   /**
    * Returns a test suite.
    *
--- moa/src/test/java/moa/classifiers/trees/HoeffdingTreeTest.java
+++ moa/src/test/java/moa/classifiers/trees/HoeffdingTreeTest.java
@@ -21,10 +21,8 @@ package moa.classifiers.trees;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
-import moa.classifiers.AbstractClassifierTestCase;
+import moa.classifiers.AbstractMultipleClassifierTestCase;
 import moa.classifiers.Classifier;
-import moa.evaluation.BasicClassificationPerformanceEvaluator;
-import moa.evaluation.ClassificationPerformanceEvaluator;
 
 /**
  * Tests the HoeffdingTree classifier.
@@ -33,7 +31,7 @@ import moa.evaluation.ClassificationPerformanceEvaluator;
  * @version $Revision$
  */
 public class HoeffdingTreeTest
-  extends AbstractClassifierTestCase {
+  extends AbstractMultipleClassifierTestCase {
 
   /**
    * Constructs the test case. Called by subclasses.
@@ -41,73 +39,9 @@ public class HoeffdingTreeTest
    * @param name 	the name of the test
    */
   public HoeffdingTreeTest(String name) {
-    super(name);
+    super(name, 2);
   }
 
-  /**
-   * Called by JUnit before each test method.
-   *
-   * @throws Exception if an error occurs.
-   */
-  @Override
-  protected void setUp() throws Exception {
-    super.setUp();
-    
-    m_TestHelper.copyResourceToTmp("naivebayes.arff");
-  }
-  
-  /**
-   * Called by JUnit after each test method.
-   *
-   * @throws Exception	if tear-down fails
-   */
-  @Override
-  protected void tearDown() throws Exception {
-    m_TestHelper.deleteFileFromTmp("naivebayes.arff");
-    
-    super.tearDown();
-  }
-
-  /**
-   * Returns the filenames (without path) of the input data files to use
-   * in the regression test.
-   *
-   * @return		the filenames
-   */
-  @Override
-  protected String[] getRegressionInputFiles() {
-    return new String[]{
-	"naivebayes.arff",
-	"naivebayes.arff",
-    };
-  }
-
-  /**
-   * Returns the class index for the datasets.
-   * 
-   * @return		the class indices (0-based)
-   */
-  @Override
-  protected int[] getRegressionInputClassIndex() {
-    return new int[]{
-	10,
-	10,
-    };
-  }
-
-  /**
-   * Returns the index of the instances in the stream to inspect the 
-   * performance/classification output of the classifiers.
-   * 
-   * @return		the inspection indices
-   */
-  @Override
-  protected int[][] getRegressionInspectionPoints() {
-    return new int[][]{
-	new int[]{10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000},
-	new int[]{10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000},
-    };
-  }
   
   /**
    * Returns the classifier setups to use in the regression test.
@@ -125,20 +59,7 @@ public class HoeffdingTreeTest
     
     return result;
   }
-
-  /**
-   * Returns the evaluator setups to use in the regression test.
-   *
-   * @return		the setups
-   */
-  @Override
-  protected ClassificationPerformanceEvaluator[] getRegressionEvaluatorSetups() {
-    return new ClassificationPerformanceEvaluator[]{
-	new BasicClassificationPerformanceEvaluator(),
-	new BasicClassificationPerformanceEvaluator(),
-    };
-  }
-
+  
   /**
    * Returns a test suite.
    *
